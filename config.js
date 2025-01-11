07c79a77600fd123471f4d07cebdc709b058c8c6
homani916 <159542043+homani916@users.noreply.github.com>
2025-01-11 05:04:18 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  bot: {
+    owners: ["1204044474854936608" , ""],// owners id
+    botID: "1249502125180981258",// bot id
+    serverinvte: `https://discord.gg/pePvxCVk`, // Server invite url
+    clientSECRET: "VNJtaGP6zwUD9TomSld-5RLox3g14tKt" , // Client Secret
+    callbackURL: "https://botbatot.the-kingking59.repl.co/login", // Call Back Url
+    TOKEN: (process.env.token) // Bot Token
+  },
+  website: {
+    PORT: "3001",//website port
+  }
+}
